effd8597cf9969d2b5380b6958a3c1ec216bcd4e
Omar Vargas <ovargas@nearsoft.com>
2012-05-28 16:07:37 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Remember Me
+
+Basado den [Remember-Me](https://github.com/mariozaizar/Remember-Me) de [@mariozaizar](https://github.com/mariozaizar)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RememberMe::Application.config.secret_token = '7932e2a716e5a46ad1ea908a51b5e67c0476ac1da2ff36d11eefe53535e97a768feaea14641de72e9d7bcae43efcc349340d5444a79b17e5bb6fb9a2e0d04ef0'
